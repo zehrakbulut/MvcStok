de998de8f5d12bb2187e0908d0e2d4888c7d8190
Zehra AKBULUT <zehraakbulut333@gmail.com>
2023-09-15 20:16:28 +0300

    Add project files.

---
+++ MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        
+        public ActionResult Index(string p)
+        {
+            var degerler = from d in db.TBLMUSTERILER select d;
+            if (!string.IsNullOrEmpty(p))
+            {
+                degerler = degerler.Where(m => m.MUSTERIAD.Contains(p));
+            }
+            return View(degerler.ToList());
+            //var degerler = db.TBLMUSTERILER.ToList();
+            //return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERILER p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniMusteri");
+            }
+            db.TBLMUSTERILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var musteri = db.TBLMUSTERILER.Find(id);
+            db.TBLMUSTERILER.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult MusteriGetir(int id)
+        {
+            var must = db.TBLMUSTERILER.Find(id);
+            return View("MusteriGetir", must);
+        }
+
+        public ActionResult Guncelle(TBLMUSTERILER p1)
+        {
+            var must = db.TBLMUSTERILER.Find(p1.MUSTERIID);
+            must.MUSTERIAD = p1.MUSTERIAD;
+            must.MUSTERISOYAD = p1.MUSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
